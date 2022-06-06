2d5985fe550ecf6a5f2e163deba218e59647f325
Dylan Luna <me@dilanluna.com>
2022-06-05 21:31:43 -0600

    chore: migrate to monorepo

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname);
+
+  const workspaceRoot = path.resolve(__dirname, '../');
+  const projectRoot = __dirname;
+
+  config.watchFolders = [workspaceRoot];
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
+  ];
 
   const { transformer, resolver } = config;
 
@@ -9,6 +19,7 @@ module.exports = (() => {
     ...transformer,
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   };
+
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
